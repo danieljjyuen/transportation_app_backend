8b357857797a4cdc121c8f21500dbb3486ad6aca
danieljjyuen <dan@LAPTOP-AP8VLLC3.localdomain>
2024-01-16 21:03:02 -0500

    parsed data from api

--- services/gtfsApiService.js
+++ services/gtfsApiService.js
@@ -1,5 +1,5 @@
 const axios = require('axios')
-
+const GtfsRealtimeBindings = require('gtfs-realtime-bindings');
 const url = 'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-l'
 const {API_KEY} = require('../utils/config')
 const headers = { 'x-api-key': API_KEY}
@@ -7,12 +7,23 @@ const headers = { 'x-api-key': API_KEY}
 //fetch from api and return data
 const testrun = async () => {
     try {
-        axios.get(url, {headers})
-            .then(response => console.log(response.data))
+        let response = await axios.get(url, {headers, responseType: 'arraybuffer' })
+    
+        //parsing data from api
+        const feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(
+          new Uint8Array(response.data)
+        );
+        feed.entity.forEach((entity) => {
+          if (entity.tripUpdate) {
+            console.log(entity.tripUpdate);
+          }
+        })
     } catch (error) {
         console.log(error)
     }
 }
+
+
 module.exports = {
     testrun
 }

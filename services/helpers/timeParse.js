1e0a75eb9352ee4e9752f4f33c2fc87fe314d089
danieljjyuen <dan@LAPTOP-AP8VLLC3.localdomain>
2024-02-27 16:07:55 -0500

    fixed time bug

--- services/helpers/timeParse.js
+++ services/helpers/timeParse.js
@@ -1,14 +1,10 @@
 //mta arrival time in seconds from jan 1st 1970, 00:00 utc
-
-
-let currentTime = new Date(new Date().toUTCString())
 let epochTime = new Date(0)
 
-//convert milliseconds -> seconds
-let currentSeconds = (currentTime - epochTime)/1000
-
-
 const timeParse = (time) => {
+    let currentTime = new Date(new Date().toUTCString())
+    //convert milliseconds -> seconds
+    let currentSeconds = (currentTime - epochTime)/1000
     return time - currentSeconds
 }
 
